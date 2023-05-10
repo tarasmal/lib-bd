69b3d1898f9f17d7abf876d1c7f3d2007b2ce33d
Taras <tarasmaliarchuk@itjet.io>
2023-05-10 21:04:53 +0300

    feat: create models

---
+++ models.js
@@ -0,0 +1,60 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+import {DataTypes} from "sequelize";
+const Application = sequelize.define('Application', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+    },
+    return_date: {
+        type: DataTypes.STRING
+    },
+    date_of_issue: {
+        type: DataTypes.STRING
+    }
+}, {
+
+});
+
+const Book = sequelize.define('Book', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+    },
+    bookTitle: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    authorsBook: {
+        type: DataTypes.STRING
+    },
+    year_of_publication: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+
+});
+const Student = sequelize.define('Student', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+    },
+    fullName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    countBook: {
+        type: DataTypes.INTEGER
+    }
+}, {
+
+});
+
+Student.hasMany(Application)
+Application.belongsTo(Application)
+
+Book.hasOne(Application)
+Application.belongsTo(Book)
+
