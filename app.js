f6b38a948bf430c50ede1a78f6cdb8b7d91652b8
Taras <tarasmaliarchuk@itjet.io>
2023-05-11 11:48:00 +0300

    feat: connect db

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
+const db = require('./db')
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+app.use(cors())
+app.use(bodyParser.json())
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
+const start = () => {
+    try {
+        db.authenticate()
+        db.sync()
+        app.listen(3000, () => {
+            console.log('server started on the port ' + 3000 )
+        })
+    }
+    catch (e){
+        console.log(e)
+    }
+}
 
-app.listen(3000)
+start()
+
